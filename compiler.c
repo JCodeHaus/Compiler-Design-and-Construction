d5a1c96d0637e302d43e204d70e0cbe54afd244f
JCodeHaus <Jareussmith012@gmail.com>
2015-03-30 16:24:02 -0500

    Update compiler.c

--- compiler.c
+++ compiler.c
@@ -1,80 +1,79 @@
-/* JAREUS SMITH*/
 #include <stdio.h>
 #include <string.h>
 // Delimiter values
 #define CONDITION " IF THEN WHILE DO BEGIN END"
 #define OPERATION " + - * / ( ) "
- 
- 
- 
- 
- void con (void)
+
+
+
+
+void con (void)
+{
+   char token[] = "IF flag THEN alpha + 2 * gamma / ( C3PO - R2D2 )";
+ char *pointers;
+
+ fprintf(stdout, "Split \"%s\" into tokens:\n",token);
+
+ pointers = strtok(token, CONDITION);
+ while(pointers != NULL)
  {
-    char token[] = "IF flag THEN alpha + 2 * gamma / ( C3PO - R2D2 )";
-  char *pointer;
  
-  fprintf(stdout, "Split \"%s\" into tokens:\n",token);
- 
-  pointer = TEMPTOKENS(token, CONDITION);
-  while(pointer != NULL)
-  {
-  
-    if (CONDITION)
-    {
-      if(pointer != OPERATION)
-      {
-      printf("RVALUE= ");
-      }
-      }
-      
+   if (CONDITION)
+   {
+     if(pointers != OPERATION)
+     {
+     printf("RVALUE= ");
+     }
+     }
      
-      
-      
     
-             fprintf(stdout, "%s\n",pointer);
-    pointers = TEMPTOKENS(NULL, CONDITION);
-    
-      
- }
- printf("LABEL= 10001\n");
- }
- 
- 
-  void op (void)
- {
-    char token[] = "IF flag THEN alpha + 2 * gamma / ( C3PO - R2D2 )";
-  char *pointer;
- 
-  
- 
-  pointer = TEMPTOKEN(token, OPERATION);
-  while(pointer != NULL)
-  {
-  
-    if (OPERATION)
-    {
-      printf("PUSH= ");
      
-      
-      
-    
-             fprintf(stdout, "%s\n",pointer);
-    pointer = TEMPTOKEN(NULL, OPERATION);
-    }
-      
- }
- 
- }
- 
+     
+   
+            fprintf(stdout, "%s\n",pointers);
+   pointers = strtok(NULL, CONDITION);
+   
+     
+}
+printf("LABEL= 10001\n");
+}
+
+
+ void op (void)
+{
+   char token[] = "IF flag THEN alpha + 2 * gamma / ( C3PO - R2D2 )";
+ char *pointer;
 
  
+
+ pointer = strtok(token, OPERATION);
+ while(pointer != NULL)
+ {
  
- 
- 
- int main (void)
+   if (OPERATION)
+   {
+     printf("PUSH= ");
+    
+     
+     
+   
+            fprintf(stdout, "%s\n",pointer);
+   pointer = strtok(NULL, OPERATION);
+   }
+     
+}
+
+}
+
+
+
+
+
+
+int main (void)
 {
-  con( );
-  op();
-  printf("Sub\nDIV\nADD\nLABEL10001\nHALT\n");
-  return 0;
+ con( );
+ op();
+ printf("Sub\nDIV\nADD\nLABEL10001\nHALT\n");
+ return 0;
 }
